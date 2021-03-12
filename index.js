71e8b3a7ea01a4675444e40aa1eb35ddcae4693b
Ritsuki Goto <develop@mizucoffee.net>
2021-03-12 21:29:00 +0900

    [fix] page width

--- index.js
+++ index.js
@@ -1,34 +1,34 @@
 const debug = false;
 
-gsap.to(".anim-1", {
-  scrollTrigger: {
-    trigger: ".anim-1",
-    start: "top top",
-    end: "50% end",
-    scrub: true,
-    pin: true,
-    markers: debug
-  }
-});
+pinPage('.anim-1', '70%')
 
-gsap.to("#pop1", {
-  scrollTrigger: {
-    trigger: ".anim-1-content",
-    start: "60% center",
-    markers: debug,
-    toggleActions: "play none none reverse"
-  },
-  opacity: 1,
-  transform: 'translateY(0px)'
-});
+show('#pop1', ".anim-1-content", "60%", "70%")
+show('#pop2', ".anim-1-content", "90%", "100%")
 
-gsap.to("#pop2", { // 動かす要素
-  scrollTrigger: {
-    trigger: ".anim-1-content",
-    start: "80% center",
-    markers: debug,
-    toggleActions: "play none none reverse"
-  },
-  opacity: 1,
-  transform: 'translateY(0px)'
-});
+function pinPage(target, size) {
+  gsap.to(target, {
+    scrollTrigger: {
+      trigger: ".anim-1",
+      start: "top top",
+      end: `${size} end`,
+      scrub: true,
+      pin: true,
+      markers: debug
+    }
+  });
+}
+
+function show(target, trigger, start, end) {
+  gsap.to(target, {
+    scrollTrigger: {
+      trigger: trigger,
+      start: `${start} center`,
+      end: `${end} center`,
+      markers: debug,
+      scrub: true,
+      toggleActions: "play none none reverse"
+    },
+    opacity: 1,
+    transform: 'translateY(0px)'
+  });
+}
