7ac1eee552edc2974f97b1a772ce473b3462fa6e
Ritsuki Goto <develop@mizucoffee.net>
2021-03-12 21:13:50 +0900

    [fix] animation

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-gsap.to(".anim-1", { // 動かす要素
+const debug = false;
+
+gsap.to(".anim-1", {
   scrollTrigger: {
-    trigger: ".anim-1", // この要素まできたらアニメーション開始
-    start: "top top", // ビューポートの設定
-    end: "100% end", // ビューポートの設定
+    trigger: ".anim-1",
+    start: "top top",
+    end: "50% end",
     scrub: true,
     pin: true,
-    markers: true // 検証用のマーカーを表示
+    markers: debug
   }
 });
 
-gsap.to("#pop1", { // 動かす要素
+gsap.to("#pop1", {
   scrollTrigger: {
-    trigger: "#pop1", // この要素まできたらアニメーション開始
-    start: "50% center", // ビューポートの設定
-    //    pin: true,
-    markers: true // 検証用のマーカーを表示
+    trigger: ".anim-1-content",
+    start: "60% center",
+    markers: debug,
+    toggleActions: "play none none reverse"
   },
   opacity: 1,
   transform: 'translateY(0px)'
@@ -22,10 +24,10 @@ gsap.to("#pop1", { // 動かす要素
 
 gsap.to("#pop2", { // 動かす要素
   scrollTrigger: {
-    trigger: "#pop2", // この要素まできたらアニメーション開始
-    start: "100% center", // ビューポートの設定
-    //    pin: true,
-    markers: true // 検証用のマーカーを表示
+    trigger: ".anim-1-content",
+    start: "80% center",
+    markers: debug,
+    toggleActions: "play none none reverse"
   },
   opacity: 1,
   transform: 'translateY(0px)'
