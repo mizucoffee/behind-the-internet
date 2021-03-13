c5f1c66bd9583cdb35aed3522751f0e5995bc8d3
Ritsuki Goto <develop@mizucoffee.net>
2021-03-14 02:31:15 +0900

    [add] anim 3

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ pinPage('#anim-1', '90%')
 show('#pop1', "#anim-1-content", "60%", "80%")
 show('#pop2', "#anim-1-content", "100%", "120%")
 
-pinPage('#anim-2', '120%')
-translate('#packet1', "#anim-2-content", "70%", "120%", 'calc(100% - 350px)')
-translate('#packet2', "#anim-2-content", "150%", "200%", '0px')
+pinPage('#anim-2', '200%')
+animate('#packet1', "#anim-2-content", "70%", "130%", {left: 'calc(100% - 350px)'})
+fadeout('#packet1', "#anim-2-content", "140%", "170%")
+fadein('#packet2', "#anim-2-content", "190%", "220%")
+animate('#packet2', "#anim-2-content", "230%", "290%", {left: '0px'})
 
-pinPage('#anim-3', '200%')
+pinPage('#anim-3', '450%')
+animate('#dns1', "#anim-3-content", "70%", "130%", {left: 'calc(100% - 350px)', top: 0})
+fadeout('#dns1', "#anim-3-content", "140%", "170%")
+
+fadein('#dns2', "#anim-3-content", "190%", "220%")
+animate('#dns2', "#anim-3-content", "230%", "290%", {left: '0px', top: 'calc(50% - 55px)'})
+fadeout('#dns2', "#anim-3-content", "300%", "330%")
+
+fadein('#dns3', "#anim-3-content", "350%", "380%")
+animate('#dns3', "#anim-3-content", "390%", "450%", {left: 'calc(100% - 350px)', bottom: 0})
+fadeout('#dns3', "#anim-3-content", "460%", "490%")
+
+fadein('#dns4', "#anim-3-content", "510%", "540%")
+animate('#dns4', "#anim-3-content", "550%", "610%", {left: '0px', bottom: 'calc(50% - 55px)'})
 
 function pinPage(target, size) {
   gsap.to(target, {
@@ -38,7 +53,7 @@ function show(target, trigger, start, end) {
   });
 }
 
-function translate(target, trigger, start, end, to) {
+function animate(target, trigger, start, end, anim) {
   gsap.to(target, {
     scrollTrigger: {
       trigger: trigger,
@@ -48,6 +63,34 @@ function translate(target, trigger, start, end, to) {
       scrub: true,
       toggleActions: "play none none reverse"
     },
-    left: to
+    ...anim
+  });
+}
+
+function fadein(target, trigger, start, end) {
+  gsap.to(target, {
+    scrollTrigger: {
+      trigger: trigger,
+      start: `${start} center`,
+      end: `${end} center`,
+      markers: true,
+      scrub: true,
+      toggleActions: "play none none reverse"
+    },
+    opacity: 1,
+  });
+}
+
+function fadeout(target, trigger, start, end) {
+  gsap.to(target, {
+    scrollTrigger: {
+      trigger: trigger,
+      start: `${start} center`,
+      end: `${end} center`,
+      markers: true,
+      scrub: true,
+      toggleActions: "play none none reverse"
+    },
+    opacity: 0,
   });
 }
