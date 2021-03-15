3021d4aa7af049f1f48e6b3a3a395d10c0cac15c
Ritsuki Goto <develop@mizucoffee.net>
2021-03-15 12:50:00 +0900

    [fix] animation

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ fadein('#packet2', "#anim-2-content", "190%", "220%")
 animate('#packet2', "#anim-2-content", "230%", "290%", {left: '0px'})
 
 pinPage('#anim-3', '450%')
-animate('#dns1', "#anim-3-content", "70%", "130%", {left: 'calc(100% - 350px)', top: 0})
+animate('#dns1', "#anim-3-content", "70%", "130%", {right: 0, top: 0})
 fadeout('#dns1', "#anim-3-content", "140%", "170%")
 
 fadein('#dns2', "#anim-3-content", "190%", "220%")
-animate('#dns2', "#anim-3-content", "230%", "290%", {left: '0px', top: 'calc(50% - 55px)'})
+animate('#dns2', "#anim-3-content", "230%", "290%", {left: 0, top: 'calc(50% - 55px)'})
 fadeout('#dns2', "#anim-3-content", "300%", "330%")
 
 fadein('#dns3', "#anim-3-content", "350%", "380%")
-animate('#dns3', "#anim-3-content", "390%", "450%", {left: 'calc(100% - 350px)', bottom: 0})
+animate('#dns3', "#anim-3-content", "390%", "450%", {right: 0, bottom: 0})
 fadeout('#dns3', "#anim-3-content", "460%", "490%")
 
 fadein('#dns4', "#anim-3-content", "510%", "540%")
-animate('#dns4', "#anim-3-content", "550%", "610%", {left: '0px', bottom: 'calc(50% - 55px)'})
+animate('#dns4', "#anim-3-content", "550%", "610%", {left: 0, bottom: 'calc(50% - 55px)'})
 
 function pinPage(target, size) {
   gsap.to(target, {
